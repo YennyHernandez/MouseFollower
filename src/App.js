ee6900598f6f4430a424a0d115ceda67627f90af
Yeni Hernandez <yeni.hernandez@udea.edu.co>
2023-06-28 20:48:44 -0500

    Logica para serguir el mouse con useEfect y useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
 import './App.css';
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'  
 
 function App() {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+  useEffect(() =>{
+    console.log('efecto', {enabled})
+    const handleMove = (event) => {  //
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+    if (enabled) {  //Escucha el envento, aqui para no rederizar siempre.
+      window.addEventListener('pointermove', handleMove)
+    }
+     // cleanup:
+    // -> cuando el componente se desmonta
+    // -> cuando cambian las dependencias, antes de ejecutar
+    //    el efecto de nuevo
+    return () => { // limpia el metodo
+      console.log('limpiezag')
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+  // [] -> solo se ejecuta una vez cuando se monta el componente
+  // [enabled] -> se ejecuta cuando cambia enabled y cuando se monta el componente
+  // undefined -> se ejecuta cada vez que se renderiza el componente
+
+  
+   
   
   return (  
-   <h3>Proyect</h3> 
-  )
+    <>
+    <div style={{
+      position: 'absolute',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      border: '1px solid #fff',
+      borderRadius: '50%',
+      opacity: 0.8,
+      pointerEvents: 'none',
+      left: -25,
+      top: -25,
+      width: 50,
+      height: 50,
+      transform: `translate(${position.x}px, ${position.y}px)`
+      
+    }}
+    />
+    <button onClick={() => setEnabled(!enabled)}>
+      {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+    </button>
+  </>
+)
 }
 export default App
